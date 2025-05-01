270f570c21f79a28acaa020dbd6749afddb63296
Gabriel Matias <ggezcom991gh@gmail.com>
2025-05-01 19:17:29 -0300

    Criando função que verifica eficiência do dano e implementando a lógica de cálculo de dano.

--- scripts/battleScripts/battle.js
+++ scripts/battleScripts/battle.js
@@ -1,178 +1,108 @@
 //919 moves
 //1025 pokemons
 
-let EnemyObj
-let AllyObj
+let EnemyStats
+let AllyStats
 
-const types = {
-    normal: {
-        strong: [],
-        weak: ["fighting"],
-        imunnity: ["ghost"]
-    },
-
-    fire: {
-        strong: ["grass", "ice", "bug", "steel"],
-        weak: ["water", "ground", "rock"],
-        imunnity: []
-    },
-
-    water:{
-        strong: ["fire", "ground", "rock"],
-        weak: ["electric", "grass"],
-        imunnity: []
-    },
-
-    grass: {
-        strong: ["water", "ground", "rock"],
-        weak: ["fire", "ice", "flying", "poison", "bug"],
-        imunnity: []
-    },
-
-    ice: {
-        strong: ["grass", "ground", "flying", "dragon"],
-        weak: ["fire", "fighting", "rock", "steel"],
-        imunnity: []
-    },
-
-    fighting: {
-        strong: ["normal", "ice", "rock", "steel", "dark"],
-        weak: ["flying", "psychic", "fairy"],
-        imunnity: []
-    },
-
-    flying: {
-        strong: ["fighting", "bug", "grass"],
-        weak: ["electric", "ice", "rock"],
-        imunnity: ["ground"]
-    },
-
-    poison: {
-        strong: ["grass", "fairy"],
-        weak: ["ground", "psychic"],
-        imunnity: []
-    },
-
-    ground: {
-        strong: ["fire", "electric", "poison", "rock", "steel"],
-        weak: ["water", "grass", "ice"],
-        imunnity: ["electric"]
-    },
-
-    psychic: {
-        strong: ["fighting", "poison"],
-        weak: ["bug", "ghost", "dark"],
-        imunnity: []
-    },
-
-    bug: {
-        strong: ["grass", "psychic", "dark"],
-        weak: ["fire", "flying", "rock"],
-        imunnity: []
-    },
-
-    rock: {
-        strong: ["fire", "ice", "flying", "bug"],
-        weak: ["water", "grass", "fighting", "ground", "steel"],
-        imunnity: []
-    },
-
-    ghost: {
-        strong: ["ghost", "psychic"],
-        weak: ["ghost", "dark"],
-        imunnity: ["normal", "fighting"]
-    },
-
-    dragon: {
-        strong: ["dragon"],
-        weak: ["ice", "dragon", "fairy"],
-        imunnity: []
-    },
-
-    dark: {
-        strong: ["psychic", "ghost"],
-        weak: ["fighting", "bug", "fairy"],
-        imunnity: []
-    },
-
-    steel: {
-        strong: ["ice", "rock", "fairy"],
-        weak: ["fire", "fighting", "ground"],
-        imunnity: ["poison"]
-    },
-
-    fairy: {
-        strong: ["fighting", "dragon", "dark"],
-        weak: ["poison", "steel"],
-        imunnity: ["dragon"]
-    },
-
-    electric: {
-        strong: ["water", "flying"],
-        weak: ["ground"],
-        imunnity: []
-    }
-
-}
-
-async function GetData() {
-    //Enemy info
+async function GetPlayersStats() {
     await axios.get(`https://pokeapi.co/api/v2/pokemon/${EnemyPokemonId}`)
     .then((response) => {
-        EnemyObj = response.data
-        return EnemyObj
+        EnemyStats = {
+            types : response.data.types,
+            hp : ((response.data.stats[0].base_stat + 31) * 2) + 110,
+            attack : ((2 * response.data.stats[1].base_stat + 31)) + 5,
+            defense : ((2 * response.data.stats[2].base_stat + 31)) + 5,
+            spAttack : ((2 * response.data.stats[3].base_stat + 31)) + 5,
+            spDefense : ((2 * response.data.stats[4].base_stat + 31)) + 5,
+            speed : ((2 * response.data.stats[5].base_stat + 31)) + 5
+        }
+        
+        return EnemyStats
     })
     .catch((error) => {
         console.error(error);
         
     })
         
-    //Ally info
     await axios.get(`https://pokeapi.co/api/v2/pokemon/${AllyPokemonId}`)
     .then((response) => {
-        console.log(response.data)
-        return AllyObj = response.data
+        AllyStats = {
+            types : response.data.types,
+            hp : ((response.data.stats[0].base_stat + 31) * 2) + 110,
+            attack : ((2 * response.data.stats[1].base_stat + 31)) + 5,
+            defense : ((2 * response.data.stats[2].base_stat + 31)) + 5,
+            spAttack : ((2 * response.data.stats[3].base_stat + 31)) + 5,
+            spDefense : ((2 * response.data.stats[4].base_stat + 31)) + 5,
+            speed : ((2 * response.data.stats[5].base_stat + 31)) + 5,
+            strongAgainst : []
+        }
+
+        return AllyStats
     })
     .catch((error) => {
         console.error(error);
         
     })
+}
+
+let DamageObj = {}
+async function DamageEfficiency(moveType, OppositeType){
+    try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/type/${moveType}`)
+        const damage_relations = response.data.damage_relations
+
+        function typeEfficiency(type){
+            if(damage_relations.double_damage_to.some(typeObj => typeObj.name === type)){
+                return 2
+            }else if(damage_relations.half_damage_to.some(typeObj => typeObj.name === type)){
+                return 0.5
+            }else if(damage_relations.no_damage_to.some(typeObj => typeObj.name === type)){
+                return 0
+            }else{
+                return 1
+            }
+        }
+
+        let totalEfficiency = 1
+        for(let type of OppositeType){
+            totalEfficiency *= typeEfficiency(type)
+        }
+        
+        return totalEfficiency
+    } catch (error) {
+        console.log(error)
+        return 404
+    }
 }
 
 async function battle() {
-    await GetData()
+    await GetPlayersStats()
 
-    //Life bars
-    var AllyLife = ((AllyObj.stats[0].base_stat + 31) * 2 + (0 / 4)) + 110
+    document.getElementById("AllyLifeBar").innerHTML = AllyStats.hp
+    document.getElementById("EnemyLifeBar").innerHTML = EnemyStats.hp
 
-    var EnemyLife = ((EnemyObj.stats[0].base_stat + 31) * 2 + (0 / 4)) + 110
+    const AllyMoveButton = document.querySelectorAll(".move")
 
-    document.getElementById("AllyLifeBar").innerHTML = AllyLife
-    document.getElementById("EnemyLifeBar").innerHTML = EnemyLife
+    AllyMoveButton.forEach(move => {
+        move.addEventListener("click", async () => {
+            await axios.get(`https://pokeapi.co/api/v2/move/${move.dataset.move}`).then((response) => {
+                DamageObj = {
+                    moveType : response.data.type.name,
+                    movePower : response.data.power,
+                    classDamage : response.data.damage_class.name,
+                }
+                console.log(DamageObj.moveType)
+                return DamageObj
+            }).catch((error) => {
+                console.log(error)
+            })
 
-    async function DamageCalc(dmg1, dmg2) {
-       await axios.get("'https://pokeapi.co/api/v2/type/6/'").then((response) => {
-        console.log(response.data)
-       }).catch((error) => {
-        console.log(error)
-       })
-    }
-
-    DamageCalc()
-
-    const move0 = document.getElementById("move0")
-
-    move0.addEventListener("click", async () => {
-        var damage
-        await axios.get(`https://pokeapi.co/api/v2/move/${move0.dataset.move}`).then((response) => {
-            console.log(response.data.power)
-            damage = response.data.power
-            return damage
-        }).catch((error) => {
-            console.log(error)
+            if(DamageObj.classDamage === "physical"){
+                var damageCount = Math.floor((((2*100/5+2) * DamageObj.movePower * (AllyStats.attack/EnemyStats.defense))/50 + 2))
+            }else if(DamageObj.classDamage === "special"){
+                var damageCount = Math.floor((((2*100/5+2) * DamageObj.movePower * (AllyStats.spAttack/EnemyStats.spDefense))/50 + 2))
+            }
         })
-
-        document.getElementById("EnemyLifeBar").innerHTML = EnemyLife - damage
     })
 }
 //Criar função de checape de final de turno
